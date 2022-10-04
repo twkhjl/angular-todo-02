48aad2bd065bb5dc81792967f54e1f556c74e11d
twkhjl <twkhjl@gmail.com>
2022-10-05 00:45:37 +0800

    modify new item textarea

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -20,16 +20,18 @@ export class SidebarComponent implements OnInit {
   is_typing:boolean=false;
 
 
+  change_height(event: any) {
 
+    event.target.style.height = 0;
+    event.target.style.height = (event.target.scrollHeight) + "px";
 
+  }
 
   updateTmpVal(event:any){
     this.is_valid=true;
     this.tmpVal=event.target.value;
-    let scrollHeight=event.target.scrollHeight;
-    event.target.style=`height:${scrollHeight}px`;
-    // console.log(event.target.scrollHeight);
   }
+
   chk_is_valid(){
     let class_list_err='ring-red-600 placeholder-red-600 focus:ring-red-600 ring-2';
     return !this.is_valid?class_list_err:''
